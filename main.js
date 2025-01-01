60deed4da3388640894ca52717dc3a5e66af34f0
Adam Jankowski <adimustbefunny@gmail.com>
2025-01-01 17:15:38 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,113 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+const releaseXatronsButton = document.querySelector('.release-xatrons');
+
+const mouse = {
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2
+}
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+
+const avatarImage = new Image();
+const avatarSize = 70
+avatarImage.src = 'Seba.png'
+
+const gameSettings = {
+    initialSpriteCount: (canvas.width / avatarSize) * (canvas.height / avatarSize) / 4
+}
+
+const gameState = {
+    score: 0,
+    sprites: []
+}
+
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+})
+
+window.addEventListener('mousemove', (e) => {
+    mouse.x = e.clientX
+    mouse.y = e.clientY
+})
+
+function randomIntFromInterval(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomNumberFromInterval(min, max) { 
+    return Math.random() * (max - min) + min;
+}
+
+function vec2(x,y){
+    this.x = x
+    this.y = y
+}
+
+function Sprite(x, y, speedMultipler, direction){
+    this.x = x
+    this.y = y
+    this.speed = 5
+    this.speedMultipler = speedMultipler
+    this.direction = direction
+    this.size = avatarSize
+    this.image = avatarImage
+}
+
+function createSprite(){
+    const x = randomIntFromInterval(0, canvas.width - avatarSize)
+    const y = randomIntFromInterval(0, canvas.height - avatarSize) 
+    const speedMultipler = randomNumberFromInterval(0.4,0.9)
+    const direction = new vec2(randomNumberFromInterval(-1,1), randomNumberFromInterval(-1, 1))
+    return new Sprite(x, y, speedMultipler, direction)
+}
+
+window.addEventListener('click', (e) => {
+    const clickedSprites = gameState.sprites.filter(s => s.x < mouse.x && s.x + s.size > mouse.x && s.y < mouse.y && s.y + s.size > mouse.y)
+
+    clickedSprites.forEach(spriteToRemove =>{
+        const idx = gameState.sprites.indexOf(spriteToRemove)
+        gameState.sprites.splice(idx, 1)
+    })
+    gameState.score += clickedSprites.length
+})
+
+releaseXatronsButton.addEventListener('click', (e) => {
+    releaseXatronsButton.disabled = true
+    startGame()
+})
+
+function startGame(){
+    for(let i = 0; i < gameSettings.initialSpriteCount; i++){
+        gameState.sprites.push(createSprite())
+    }
+
+    window.requestAnimationFrame(loop)
+}
+
+function loop(){
+    c.clearRect(0,0,canvas.width,canvas.height)
+    c.fillText('HTML Canvas', mouse.x, mouse.y)
+
+    gameState.sprites.forEach(sprite => {
+        sprite.x += (sprite.speed ) * sprite.direction.x
+        sprite.y += (sprite.speed ) * sprite.direction.y
+
+        if(sprite.y < 0 || sprite.y > canvas.height - avatarSize){
+            sprite.direction.y = -sprite.direction.y
+        } 
+        if(sprite.x < 0 || sprite.x > canvas.width - avatarSize){
+            sprite.direction.x = -sprite.direction.x
+        } 
+
+        c.drawImage(sprite.image, sprite.x, sprite.y)
+    });
+
+    window.requestAnimationFrame(loop)
+}
+
